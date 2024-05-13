602edd83934c55183aeb9bf3ebde26f1193b7037
Phillip Schaefer <2212535@stud.hs.mannheim.de>
2024-05-13 14:48:33 +0200

    added api and initialize_module

--- src/nks/hcvault/provider.rs
+++ src/nks/hcvault/provider.rs
@@ -93,12 +93,42 @@ impl NksProvider {
     #[instrument]
     fn initialize_module(
         &mut self,
-        address: address,
+        nks_address: nks_address,
+        nks_token: nks_token,
         key_algorithm: AsymmetricEncryption,
         sym_algorithm: Option<BlockCiphers>,
         hash: Option<Hash>,
         key_usages: Vec<KeyUsage>,
     ) -> Result<(), SecurityModuleError> {
+        self.nks_address = nks_address;
+        self.nks_token = nks_token;
+        self.key_algorithm = Some(key_algorithm);
+        self.sym_algorithm = sym_algorithm;
+        self.hash = hash;
+        self.key_usages = Some(key_usages);
+        // Check if tokens file exists
+        let tokens_file_path = "path/to/tokens_file"; // Adjust the path as needed
+        if Path::new(&tokens_file_path).exists() {
+            println!("Tokens file exists.");
+            // Tokens file exists, do something
+        } else {
+            println!("Tokens file does not exist. Generating tokens...");
+            // Tokens file does not exist, generate tokens using API
+            match api::get_token() {
+                Ok(tokens) => {
+                    // Save tokens to file
+                    if let Err(err) = save_tokens_to_file(&tokens_file_path, &tokens) {
+                        println!("Failed to save tokens to file: {}", err);
+                        return Err(SecurityModuleError::TokenFileError);
+                    }
+                    println!("Token safed sucessfully");
+                }
+                Err(err) => {
+                    println!("Failed to get tokens from API: {}", err);
+                    return Err(SecurityModuleError::TokenGenerationError);
+                }
+            }
+        }
         Ok(())
     }
 }
