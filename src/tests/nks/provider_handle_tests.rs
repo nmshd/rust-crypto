73ee0148a260c1da4837af2b6662bb6b4a2f6510
pl-CJump <3000986@stud.hs-mannheim.de>
2024-05-23 09:40:19 +0200

    reduce duplicate code in provider_handle_tests + impl test_load_ecdsa_key and test_load_ecdh_key

--- src/tests/nks/provider_handle_tests.rs
+++ src/tests/nks/provider_handle_tests.rs
@@ -1,8 +1,4 @@
-use std::path::Path;
-use std::str::FromStr;
-use futures::future::ok;
-use reqwest::Url;
-#[allow(unused_imports)]
+use std::sync::Arc;
 use crate::{
     common::{
         crypto::{
@@ -17,9 +13,7 @@ use crate::{
     },
     nks::hcvault::NksProvider,
 };
-use crate::common::crypto::algorithms::encryption::SymmetricMode;
 use crate::common::crypto::algorithms::hashes::Sha2Bits;
-use crate::common::crypto::algorithms::KeyBits;
 use crate::common::traits::module_provider_config::ProviderConfig;
 use crate::nks::NksConfig;
 
@@ -33,46 +27,19 @@ fn do_nothing() {
 fn test_initialize_module() {
     let mut provider = NksProvider::new("test_key".to_string());
 
-    //set config
-    let config= NksConfig::new(
-        "".to_string(),
-        "http://localhost:5272/apidemo/".to_string(),
-        AsymmetricEncryption::Rsa(2048.into()),
-        Hash::Sha2(256.into()),
-        vec![
-            KeyUsage::ClientAuth,
-            KeyUsage::Decrypt,
-            KeyUsage::SignEncrypt,
-            KeyUsage::CreateX509,
-        ]
-    );
-    provider.config = Some(config);
+    provider.config = Some(get_config("rsa").unwrap());
 
     provider
         .initialize_module()
         .expect("Failed to initialize module");
     println!("{:?}", provider);
-    ok::<(), ()>(());
 }
 
 #[test]
 fn test_create_rsa_key() {
     let mut provider = NksProvider::new("test_key".to_string());
 
-    //set config
-    let config= NksConfig::new(
-        "".to_string(),
-        "http://localhost:5272/apidemo/".to_string(),
-        AsymmetricEncryption::Rsa(2048.into()),
-        Hash::Sha2(256.into()),
-        vec![
-            KeyUsage::ClientAuth,
-            KeyUsage::Decrypt,
-            KeyUsage::SignEncrypt,
-            KeyUsage::CreateX509,
-        ]
-    );
-    provider.config = Some(config);
+    provider.config = Some(get_config("rsa").unwrap());
 
     provider
         .initialize_module()
@@ -91,15 +58,7 @@ fn test_create_rsa_key() {
 fn test_create_ecdsa_key() {
     let mut provider = NksProvider::new("test_key".to_string());
 
-    //set config
-    let config= NksConfig::new(
-        "".to_string(),
-        "http://localhost:5272/apidemo/".to_string(),
-        AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDsa(EccCurves::Curve25519)),
-        Hash::Sha2(Sha2Bits::Sha256),
-        vec![KeyUsage::SignEncrypt, KeyUsage::ClientAuth],
-    );
-    provider.config = Some(config);
+    provider.config = Some(get_config("ecdsa").unwrap());
 
     provider
         .initialize_module()
@@ -119,15 +78,7 @@ fn test_create_ecdsa_key() {
 fn test_create_ecdh_key() {
     let mut provider = NksProvider::new("test_key".to_string());
 
-    //set config
-    let config= NksConfig::new(
-        "".to_string(),
-        "http://localhost:5272/apidemo/".to_string(),
-        AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDh(EccCurves::Curve25519)),
-        Hash::Sha2(384.into()),
-        vec![KeyUsage::Decrypt],
-    );
-    provider.config = Some(config);
+    provider.config = Some(get_config("ecdh").unwrap());
 
     provider
         .initialize_module()
@@ -146,22 +97,7 @@ fn test_create_ecdh_key() {
 fn test_load_rsa_key() {
     let mut provider = NksProvider::new("test_key".to_string());
 
-    //set config
-    //before executing this test, make sure you have created a key with the name "test_rsa_key" in the NKS server
-    //then put the current token in the config
-    let config= NksConfig::new(
-        "".to_string(),
-        "http://localhost:5272/apidemo/".to_string(),
-        AsymmetricEncryption::Rsa(2048.into()),
-        Hash::Sha2(256.into()),
-        vec![
-            KeyUsage::ClientAuth,
-            KeyUsage::Decrypt,
-            KeyUsage::SignEncrypt,
-            KeyUsage::CreateX509,
-        ]
-    );
-    provider.config = Some(config);
+    provider.config = Some(get_config("rsa").unwrap());
 
     provider
         .initialize_module()
@@ -170,43 +106,78 @@ fn test_load_rsa_key() {
     if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
         provider
             .load_key("test_rsa_key", Box::new(nks_config.clone()))
-            .expect("Failed to create RSA key");
+            .expect("Failed to load RSA key");
     } else {
-        println!("Failed to load RSA key");
+        println!("Failed to downcast to NksConfig");
     }
 }
-//
-// #[test]
-// fn test_load_ecdsa_key() {
-//     let mut provider = NksProvider::new("test_key".to_string());
-//
-//     let key_algorithm = AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDsa(EccCurves::Curve25519));
-//     let sym_algorithm = None;
-//     let hash = Some(Hash::Sha2(256.into()));
-//     let key_usages = vec![KeyUsage::ClientAuth, KeyUsage::SignEncrypt];
-//
-//     provider
-//         .initialize_module(999999,999999, key_algorithm.clone(), sym_algorithm.clone(), hash.clone(), key_usages.clone())
-//         .expect("Failed to initialize module");
-//
-//     provider
-//         .load_key("test_ecdsa_key")
-//         .expect("Failed to load ECDSA key");
-// }
-//
-// #[test]
-// fn test_load_ecdh_key() {
-//     let mut provider = NksProvider::new("test_key".to_string());
-//
-//     let key_algorithm = AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDh(EccCurves::Curve25519));
-//     let sym_algorithm = Some(BlockCiphers::Aes(Default::default(), 256.into()));
-//     let hash = Some(Hash::Sha2(384.into()));
-//     let key_usages = vec![KeyUsage::Decrypt];
-//
-//     provider
-//         .initialize_module(999999,999999, key_algorithm.clone(), sym_algorithm.clone(), hash.clone(), key_usages.clone())
-//         .expect("Failed to initialize module");
-//     provider
-//         .load_key("test_ecdh_key")
-//         .expect("Failed to load ECDH key");
-// }
+
+#[test]
+fn test_load_ecdsa_key() {
+    let mut provider = NksProvider::new("test_key".to_string());
+
+    provider.config = Some(get_config("ecdsa").unwrap());
+
+    provider
+        .initialize_module()
+        .expect("Failed to initialize module");
+
+    if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
+        provider
+            .load_key("test_ecdsa_key", Box::new(nks_config.clone()))
+            .expect("Failed to load ECDSA key");
+    } else {
+        println!("Failed to downcast to NksConfig");
+    }
+}
+
+#[test]
+fn test_load_ecdh_key() {
+    let mut provider = NksProvider::new("test_key".to_string());
+
+    provider.config = Some(get_config("ecdh").unwrap());
+
+    provider
+        .initialize_module()
+        .expect("Failed to initialize module");
+
+    if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
+        provider
+            .load_key("test_ecdh_key", Box::new(nks_config.clone()))
+            .expect("Failed to load ECDH key");
+    } else {
+        println!("Failed to downcast to NksConfig");
+    }
+}
+
+fn get_config(key_type: &str) -> Option<Arc<dyn ProviderConfig+Send+Sync>> {
+    match key_type {
+        "rsa" => Some(NksConfig::new(
+            "".to_string(),
+            "http://localhost:5272/apidemo/".to_string(),
+            AsymmetricEncryption::Rsa(2048.into()),
+            Hash::Sha2(256.into()),
+            vec![
+                KeyUsage::ClientAuth,
+                KeyUsage::Decrypt,
+                KeyUsage::SignEncrypt,
+                KeyUsage::CreateX509,
+            ]
+        )),
+        "ecdsa" => Some(NksConfig::new(
+            "".to_string(),
+            "http://localhost:5272/apidemo/".to_string(),
+            AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDsa(EccCurves::Curve25519)),
+            Hash::Sha2(Sha2Bits::Sha256),
+            vec![KeyUsage::SignEncrypt, KeyUsage::ClientAuth],
+        )),
+        "ecdh" => Some(NksConfig::new(
+            "".to_string(),
+            "http://localhost:5272/apidemo/".to_string(),
+            AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDh(EccCurves::Curve25519)),
+            Hash::Sha2(384.into()),
+            vec![KeyUsage::Decrypt],
+        )),
+        _ => None,
+    }
+}
