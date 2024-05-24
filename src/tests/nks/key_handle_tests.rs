cce73cb9286124103879c32322dea547bb253275
Phillip Schaefer <2212535@stud.hs-mannheim.de>
2024-05-24 14:01:33 +0200

    refactored test_sign_and_verify_rsa and added test_encrypt_and_decrypt_rsa

--- src/tests/nks/key_handle_tests.rs
+++ src/tests/nks/key_handle_tests.rs
@@ -43,17 +43,24 @@ fn do_nothing() {
 
  #[test]
  fn test_sign_and_verify_rsa() {
-     let mut provider = NksProvider::new("test_rsa_key".to_string());
+     let mut provider = NksProvider::new("test_key".to_string());
 
-     let config = NksConfig::new(
-         "test_token".to_string(),
-         "test_address".to_string(),
-         AsymmetricEncryption::Rsa(KeyBits::Bits4096),
-         Hash::Sha2(Sha2Bits::Sha256),
-         vec![KeyUsage::SignEncrypt, KeyUsage::ClientAuth],
-     );
-     let config_boxed: Box<dyn ProviderConfig> = Box::new(config);
-     provider.create_key("test_key1", config_boxed);
+     provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("rsa").unwrap());
+
+     provider
+         .initialize_module()
+         .expect("Failed to initialize module");
+
+     if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
+         println!("NKS Token: {}", nks_config.nks_token);
+         println!("NKS Address: {}", nks_config.nks_address);
+         provider
+             .load_key("test_rsa_key", Box::new(nks_config.clone()))
+             .expect("Failed to load RSA key");
+     } else {
+         println!("Failed to downcast to NksConfig");
+     }
+
      let data = b"Hello, World!";
      let signature = provider.sign_data(data).expect("Failed to sign data");
      assert!(provider.verify_signature(data, &signature,).unwrap());
@@ -81,30 +88,24 @@ fn do_nothing() {
 //     assert!(provider.verify_signature(data, &signature, "").unwrap());
 // }
 //
-// #[test]
-// fn test_encrypt_and_decrypt_rsa() {
-//     let mut provider = NksProvider::new("test_rsa_key".to_string());
-//
-//     let key_algorithm = AsymmetricEncryption::Rsa(2048.into());
-//     let sym_algorithm = None;
-//     let hash = Some(Hash::Sha2(256.into()));
-//     let key_usages = vec![KeyUsage::Decrypt, KeyUsage::SignEncrypt];
-//
-//     provider
-//         .initialize_module(999999,999999, key_algorithm.clone(), sym_algorithm.clone(), hash.clone(), key_usages.clone())
-//         .expect("Failed to initialize module");
-//     provider
-//         .create_key("test_rsa_key", key_algorithm.clone(), sym_algorithm.clone(), hash, key_usages)
-//         .expect("Failed to create RSA key");
-//
-//     let data = b"Hello, World!";
-//     let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-//     let decrypted_data = provider
-//         .decrypt_data(&encrypted_data)
-//         .expect("Failed to decrypt data");
-//
-//     assert_eq!(data, decrypted_data.as_slice());
-// }
+ #[test]
+ fn test_encrypt_and_decrypt_rsa() {
+    let mut provider = NksProvider::new("test_key".to_string());
+
+    provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("rsa").unwrap());
+
+    provider
+        .initialize_module()
+        .expect("Failed to initialize module");
+
+     let data = b"Hello, World!";
+     let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
+     let decrypted_data = provider
+         .decrypt_data(&encrypted_data)
+         .expect("Failed to decrypt data");
+
+     assert_eq!(data, decrypted_data.as_slice());
+}
 //
 // #[test]
 // fn test_encrypt_and_decrypt_ecdh() {
