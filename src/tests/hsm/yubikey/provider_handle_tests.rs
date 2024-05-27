6c69b2b1c1803acce0720aaf5707c27a8032d271
jannis <steinj2311@gmail.com>
2024-05-27 17:55:48 +0200

    push für test
    Co-authored-by: Eisberch <Eisberch@users.noreply.github.com>

--- src/tests/hsm/yubikey/provider_handle_tests.rs
+++ src/tests/hsm/yubikey/provider_handle_tests.rs
@@ -25,40 +25,22 @@ fn test_create_rsa_key() {
         .initialize_module()
         .expect("Failed to initialize module");
     provider
-        .create_key(config)
+        .create_key(self.key_id, config)
         .expect("Failed to create RSA key");
 }
 
 #[test]
-fn test_create_ecdsa_key() {
-    let mut provider = YubiKeyProvider::new("test_rsa_key".to_string());
+fn test_create_ecc_key() {
+    let mut provider = YubiKeyProvider::new("test_ecc_key".to_string());
 
-    let config = ProviderConfig::new(AsymmetricEncryption::Rsa, Some(KeyUsage::SignEncrypt));
+    let config = ProviderConfig::new(AsymmetricEncryption::Ecc, Some(KeyUsage::SignEncrypt));
 
     provider
         .initialize_module()
         .expect("Failed to initialize module");
     provider
-        .create_key("test_rsa_key", config)
+        .create_key(self.key_id, config)
         .expect("Failed to create RSA key");
-}
-
-#[test]
-fn test_create_ecdh_key() {
-    let mut provider = TpmProvider::new("test_ecdh_key".to_string());
-
-    let config = TpmConfig::new(
-        AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDh(EccCurves::Curve25519)),
-        BlockCiphers::Aes(SymmetricMode::Gcm, KeyBits::Bits512),
-        Hash::Sha2(Sha2Bits::Sha256),
-        vec![KeyUsage::SignEncrypt, KeyUsage::Decrypt],
-    );
-    provider
-        .initialize_module()
-        .expect("Failed to initialize module");
-    provider
-        .create_key("test_ecdh_key", config)
-        .expect("Failed to create ECDH key");
 }
 
 #[test]
