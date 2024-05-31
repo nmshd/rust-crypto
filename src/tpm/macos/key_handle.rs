9904dd206fb0c0cf5f8f0852a19ea885c46c0ede
Skyisntthelimit <73102067+Skyisntthelimit@users.noreply.github.com>
2024-05-31 12:36:48 +0200

    Error handling implemented

--- src/tpm/macos/key_handle.rs
+++ src/tpm/macos/key_handle.rs
@@ -4,21 +4,46 @@ impl KeyHandle for TpmProvider {
     fn sign_data(&self, data: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
         let string_data = String::from_utf8(data.to_vec()).map_err(|_| SecurityModuleError::SigningError("Data conversion error".to_string()))?;
         let signed_data = ffi::rustcall_sign_data(string_data, "3344".to_string());
-        Ok(signed_data.into_bytes())
+
+        
+        // The FFI bridge always returns strings by design.
+        // Therefore, we need to search for the case-insensitive string "error".
+        // If "error" is found, we return an error to the function.
+        if Regex::new("(?i)error").unwrap().is_match(signed_data.as_str()) {
+            Err(SecurityModuleError::EncryptionError(signed_data.to_string()))
+        } else {
+            Ok(signed_data.into_bytes())
+        }
     }
 
     #[instrument] 
     fn decrypt_data(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
         let string_data = String::from_utf8(encrypted_data.to_vec()).map_err(|_| SecurityModuleError::DecryptionError("Data conversion error".to_string()))?;
         let decrypted_data = ffi::rustcall_decrypt_data(string_data, "3344".to_string());
-        Ok(decrypted_data.into_bytes())
+    
+        // The FFI bridge always returns strings by design.
+        // Therefore, we need to search for the case-insensitive string "error".
+        // If "error" is found, we return an error to the function.
+        if Regex::new("(?i)error").unwrap().is_match(decrypted_data.as_str()) {
+            Err(SecurityModuleError::EncryptionError(decrypted_data.to_string()))
+        } else {
+            Ok(decrypted_data.into_bytes())
+        }
     }
 
     #[instrument] 
     fn encrypt_data(&self, data: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
         let string_data = String::from_utf8(data.to_vec()).map_err(|_| SecurityModuleError::EncryptionError("Data conversion error".to_string()))?;
         let encrypted_data = ffi::rustcall_encrypt_data(string_data, "3344".to_string());
-        Ok(encrypted_data.into_bytes())
+
+        // The FFI bridge always returns strings by design.
+        // Therefore, we need to search for the case-insensitive string "error".
+        // If "error" is found, we return an error to the function.
+        if Regex::new("(?i)error").unwrap().is_match(encrypted_data.as_str()) {
+            Err(SecurityModuleError::EncryptionError(encrypted_data.to_string()))
+        } else {
+            Ok(encrypted_data.into_bytes())
+        }
     }
 
     #[instrument] 
@@ -27,13 +52,17 @@ impl KeyHandle for TpmProvider {
         let string_signature = String::from_utf8(signature.to_vec()).map_err(|_| SecurityModuleError::SignatureVerificationError("Signature conversion error".to_string()))?;
 
         let verification_result = ffi::rustcall_verify_data(string_data, string_signature, "3344".to_string());
+
         
+        // The FFI bridge always returns strings by design.
+        // If not "true" or "false" is found, we expect an error from the function
         match verification_result.as_str() {
             "true" => Ok(true),
             "false" => Ok(false),
-            _ => Err(SecurityModuleError::SignatureVerificationError("Failed to verify the signature".to_string())),
+            _ => Err(SecurityModuleError::SignatureVerificationError(verification_result.as_str())),
         }
     }
+
 }
 
 #[swift_bridge::bridge]
