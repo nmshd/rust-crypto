acc3198331a488a798284e6ee7baa0e9aeb906d9
3003991 <118073792+HS3003991@users.noreply.github.com>
2024-05-31 17:16:19 +0200

    Preparing pullrequest error.rs

--- src/tpm/macos/main/codesigning.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-codesign -f -s "F5C0AD229021961EF94754EE76F28E2CD538545D" --entitlements "binaryknights.entitlements" -o runtime -i "de.jssoft.BinaryKnights" "target/debug/rust-binary-calls-swift-package"
