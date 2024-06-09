eb6aa5dc153a5949a8c008cfeedf15934f339b0f
pl-CJump <3000986@stud.hs-mannheim.de>
2024-06-09 15:35:12 +0200

    restructure aes key_handle tests to reduce duplicate code

--- src/tests/nks/key_handle_tests.rs
+++ src/tests/nks/key_handle_tests.rs
@@ -120,193 +120,47 @@ fn test_encrypt_and_decrypt_ecdh() {
 
 #[test]
 fn test_encrypt_and_decrypt_aes_gcm() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Gcm;
-        let mut provider = NksProvider::new("aes_gcm".to_string());
-
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
-
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
-
-        if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
-                .expect("Failed to load AES key");
-        } else {
-            println!("Failed to downcast to NksConfig");
-        }
-
-        let data = b"Hello, World!";
-        let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
-        assert_eq!(data, decrypted_data.as_slice())
-    }
+    test_encrypt_and_decrypt_aes(SymmetricMode::Gcm, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
 }
 
 #[test]
 fn test_encrypt_and_decrypt_aes_ccm() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Ccm;
-        let mut provider = NksProvider::new("aes_ccm".to_string());
-
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
-
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
-
-        if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
-                .expect("Failed to load AES key");
-        } else {
-            println!("Failed to downcast to NksConfig");
-        }
-
-        let data = b"Hello, World!";
-        let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
-        assert_eq!(data, decrypted_data.as_slice())
-    }
+    test_encrypt_and_decrypt_aes(SymmetricMode::Ccm, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
 }
 
 #[test]
 fn test_encrypt_and_decrypt_aes_ecb() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Ecb;
-        let mut provider = NksProvider::new("aes_ecb".to_string());
-
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
-
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
-
-        if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
-                .expect("Failed to load AES key");
-        } else {
-            println!("Failed to downcast to NksConfig");
-        }
-
-        let data = b"Hello, World!";
-        let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
-        assert_eq!(data, decrypted_data.as_slice())
-    }
+    test_encrypt_and_decrypt_aes(SymmetricMode::Ecb, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
 }
 
 #[test]
 fn test_encrypt_and_decrypt_aes_cbc() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Cbc;
-        let mut provider = NksProvider::new("aes_cbc".to_string());
-
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
-
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
-
-        if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
-                .expect("Failed to load AES key");
-        } else {
-            println!("Failed to downcast to NksConfig");
-        }
-
-        let data = b"Hello, World!";
-        let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
-        assert_eq!(data, decrypted_data.as_slice())
-    }
-}
-
-#[test]
-fn test_encrypt_and_decrypt_aes_ctr() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Ctr;
-        let mut provider = NksProvider::new("aes_ctr".to_string());
-
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
-
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
-
-        if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
-                .expect("Failed to load AES key");
-        } else {
-            println!("Failed to downcast to NksConfig");
-        }
-
-        let data = b"Hello, World!";
-        let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
-        assert_eq!(data, decrypted_data.as_slice())
-    }
+    test_encrypt_and_decrypt_aes(SymmetricMode::Cbc, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
 }
 
 #[test]
 fn test_encrypt_and_decrypt_aes_cfb() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Cfb;
-        let mut provider = NksProvider::new("aes_cfb".to_string());
-
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
-
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
-
-        if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
-                .expect("Failed to load AES key");
-        } else {
-            println!("Failed to downcast to NksConfig");
-        }
-
-        let data = b"Hello, World!";
-        let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
-        assert_eq!(data, decrypted_data.as_slice())
-    }
+    test_encrypt_and_decrypt_aes(SymmetricMode::Cfb, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
 }
 
 #[test]
 fn test_encrypt_and_decrypt_aes_ofb() {
-    for &key_size in &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256] {
-        let aes_mode = SymmetricMode::Ofb;
-        let mut provider = NksProvider::new("aes_ofb".to_string());
+    test_encrypt_and_decrypt_aes(SymmetricMode::Ofb, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
+}
 
-        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(aes_mode)).unwrap());
+#[test]
+fn test_encrypt_and_decrypt_aes_ctr() {
+    test_encrypt_and_decrypt_aes(SymmetricMode::Ctr, &[KeyBits::Bits128, KeyBits::Bits192, KeyBits::Bits256]);
+}
 
-        provider
-            .initialize_module()
-            .expect("Failed to initialize module");
+fn test_encrypt_and_decrypt_aes(mode: SymmetricMode, key_sizes: &[KeyBits]) {
+    for &key_size in key_sizes {
+        let mut provider = NksProvider::new(format!("aes_{}", mode as u8));
+        provider.config = Some(crate::tests::nks::provider_handle_tests::get_config("aes", Some(key_size), Some(mode)).unwrap());
+        provider.initialize_module().expect("Failed to initialize module");
 
         if let Some(nks_config) = provider.config.as_ref().unwrap().as_any().downcast_ref::<NksConfig>() {
-            provider
-                .load_key(&format!("test_aes_key_{}_{}", aes_mode as u8, key_size as u8), Box::new(nks_config.clone()))
+            provider.load_key(&format!("test_aes_key_{}_{}", mode as u8, key_size as u8), Box::new(nks_config.clone()))
                 .expect("Failed to load AES key");
         } else {
             println!("Failed to downcast to NksConfig");
@@ -314,9 +168,7 @@ fn test_encrypt_and_decrypt_aes_ofb() {
 
         let data = b"Hello, World!";
         let encrypted_data = provider.encrypt_data(data).expect("Failed to encrypt data");
-        let decrypted_data = provider
-            .decrypt_data(&encrypted_data)
-            .expect("Failed to decrypt data");
+        let decrypted_data = provider.decrypt_data(&encrypted_data).expect("Failed to decrypt data");
         assert_eq!(data, decrypted_data.as_slice())
     }
 }
