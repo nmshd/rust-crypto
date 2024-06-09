96016e9c27e17de5fb1fa4af27aca91cb094af8f
The_KTC <91944251+The-KTC@users.noreply.github.com>
2024-06-09 11:22:54 +0200

    logger is done

--- src/tpm/macos/logger.rs
+++ src/tpm/macos/logger.rs
@@ -1,9 +1,28 @@
+use tracing::Level;
+use tracing_appender::rolling;
+use tracing_subscriber::FmtSubscriber;
 use crate::common::traits::log_config::LogConfig;
-#[derive(Debug)]
-pub struct SecureEnclaveLogger; 
 
-impl LogConfig for SecureEnclaveLogger{
-    fn setup_logging(&self){
-        println!("Logger muss noch erstellt und implementiert werden!"); 
+
+#[derive(Debug, Clone, Copy)]
+pub struct Logger {}
+
+impl LogConfig for Logger{
+    fn setup_logging(&self) {
+        let file_appender = rolling::daily("./logs", "output.log");
+        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(Level::TRACE)
+            .with_writer(non_blocking)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
     }
 }
+
+impl Logger {
+    pub fn new_boxed() -> Box<dyn LogConfig> {
+        Box::new(Self {})
+    }
+}
+
