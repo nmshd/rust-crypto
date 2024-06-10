91b81eb27661e130927c17103f40ef8e97a55061
Eisberch <3000430@stud.hs-mannheim.de>
2024-06-10 16:34:34 +0200

    Schlüssel generieren in der Demo App hinzugefügt

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,7 @@ fn main() -> glib::ExitCode {
 
         let list_box = ListBox::new(); // Erstelle eine neue ListBox
         let actions = vec![
+            "Schlüssel generieren",
             "Daten verschlüsseln",
             "Daten entschlüsseln",
             "Daten signieren",
@@ -60,10 +61,11 @@ fn main() -> glib::ExitCode {
         list_box.connect_row_activated(move |_, row| {
             let index = row.index();
             match index {
-                0 => create_new_window(&app_clone, "Daten verschlüsseln".to_string()),
-                1 => create_new_window(&app_clone, "Daten entschlüsseln".to_string()),
-                2 => create_new_window(&app_clone, "Daten signieren".to_string()),
-                3 => create_new_window(&app_clone, "Signatur verifizieren".to_string()),
+                0 => create_new_window(&app_clone, "Schlüssel generieren".to_string()),
+                1 => create_new_window(&app_clone, "Daten verschlüsseln".to_string()),
+                2 => create_new_window(&app_clone, "Daten entschlüsseln".to_string()),
+                3 => create_new_window(&app_clone, "Daten signieren".to_string()),
+                4 => create_new_window(&app_clone, "Signatur verifizieren".to_string()),
                 _ => {}
             }
         });
@@ -79,7 +81,8 @@ fn main() -> glib::ExitCode {
 }
 
 fn create_new_window(app: &Application, title: String) {
-    let new_window = ApplicationWindow::builder()
+    if title != "Schlüssel generieren" {
+let new_window = ApplicationWindow::builder()
         .application(app)
         .title(title.clone())
         .default_width(400)
@@ -125,6 +128,52 @@ fn create_new_window(app: &Application, title: String) {
 
     new_window.set_child(Some(&vbox));
     new_window.present();
+    } else {
+    let new_window = ApplicationWindow::builder()
+        .application(app)
+        .title(title.clone())
+        .default_width(400)
+        .default_height(300)
+        .build();
+
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 5);
+    vbox.set_spacing(5);
+
+    let label_key_id = Label::new(Some("Key ID:"));
+    let entry_key_id = Entry::new();
+    let entry_key_id_clone = entry_key_id.clone();
+
+    
+    let label_encryption_type = Label::new(Some("Welchen Schlüssel wollen sie generieren?"));
+    let combo_encryption_type = gtk::ComboBoxText::new();
+    combo_encryption_type.append(None, "RSA1024");
+    combo_encryption_type.append(None, "RSA2048");
+    combo_encryption_type.append(None, "ECC256");
+    combo_encryption_type.append(None, "ECC384");
+    combo_encryption_type.set_active(Some(0)); // Standardmäßig den ersten Eintrag aktivieren
+    let combo_encryption_type_clone = combo_encryption_type.clone();
+
+    let button = Button::with_label("Generieren");
+    let app2 = app.clone();
+    
+    let data = " ";
+
+    button.connect_clicked(move |_| {
+        let key_id = entry_key_id.text().to_string();
+        let encryption_type = combo_encryption_type.active_text().unwrap().to_string();
+        perform_action(&app2,&title, &data, &key_id, &encryption_type);
+    });
+
+    vbox.append(&label_key_id);
+    vbox.append(&entry_key_id_clone);
+    vbox.append(&label_encryption_type);
+    vbox.append(&combo_encryption_type_clone);
+    vbox.append(&button);
+
+    new_window.set_child(Some(&vbox));
+    new_window.present();
+    }
+    
 }
 
 
@@ -132,6 +181,9 @@ fn perform_action(app: &Application, action: &str, data: &str, key_id: &str, enc
     match action {
         //   "Daten verschlüsseln" => encrypt_data(data, key_id, encryption_type),
         //   "Daten entschlüsseln" => decrypt_data(data, key_id, encryption_type),
+        "Schlüssel generieren" => {
+            generate(app, encryption_type, key_id);
+        }
         "Daten signieren" => {
             let ergebnis = sign_data(data, key_id, encryption_type);
             match ergebnis {
@@ -272,8 +324,8 @@ fn sign_data(
         .initialize_module()
         .expect("Failed to initialize module");
     provider
-        .create_key(key_id, config)
-        .expect("Failed to create RSA key");
+        .load_key(key_id, config)
+        .expect("Failed to load key");
     let data: &[u8] = data.trim().as_bytes();
     let signature = provider.sign_data(data);
     match signature {
@@ -302,4 +354,106 @@ fn create_new_window2(app: &Application, message: String) {
 
     new_window.set_child(Some(&vbox));
     new_window.present();
+}
+
+fn generate(app: &Application, encryption_type: &str, key_id: &str) {
+    let mut provider = YubiKeyProvider::new(key_id.to_string());
+    let mut config = HsmProviderConfig::new(
+        AsymmetricEncryption::Rsa(KeyBits::Bits1024),
+        vec![KeyUsage::SignEncrypt],
+    );
+    match encryption_type {
+        "RSA1024" => {
+            config = HsmProviderConfig::new(
+                AsymmetricEncryption::Rsa(KeyBits::Bits1024),
+                vec![KeyUsage::SignEncrypt],
+            );
+        }
+        "RSA2048" => {
+            config = HsmProviderConfig::new(
+                AsymmetricEncryption::Rsa(KeyBits::Bits2048),
+                vec![KeyUsage::SignEncrypt],
+            );
+        }
+        "ECC256" => {
+            config = HsmProviderConfig::new(
+                AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDsa(EccCurves::P256)),
+                vec![KeyUsage::SignEncrypt],
+            );
+        }
+        "ECC384" => {
+            config = HsmProviderConfig::new(
+                AsymmetricEncryption::Ecc(EccSchemeAlgorithm::EcDsa(EccCurves::P384)),
+                vec![KeyUsage::SignEncrypt],
+            );
+        }
+        _ => {}
+    }
+
+    provider
+        .initialize_module()
+        .expect("Failed to initialize module");
+    match encryption_type {
+        "RSA1024" => {
+            let rsa = provider
+                .create_key(key_id, config);
+            match rsa {
+                Ok(_) => {
+                    let ausgabe = "RSA1024 Schlüssel erfolgreich generiert";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+                Err(_) => {
+                    let ausgabe = "Fehler beim generieren des RSA1024 Schlüssels";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+            
+            }
+        }
+        "RSA2048" => {
+            let rsa = provider
+                .create_key(key_id, config);
+            match rsa {
+                Ok(_) => {
+                    let ausgabe = "RSA2048 Schlüssel erfolgreich generiert";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+                Err(_) => {
+                    let ausgabe = "Fehler beim generieren des RSA2048 Schlüssels";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+            
+            }
+        }
+        "ECC256" => {
+            let ecc = provider
+                .create_key(key_id, config);
+            match ecc {
+                Ok(_) => {
+                    let ausgabe = "ECC256 Schlüssel erfolgreich generiert";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+                Err(_) => {
+                    let ausgabe = "Fehler beim generieren des ECC256 Schlüssels";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+            
+            }
+        }
+        "ECC384" => {
+            let ecc = provider
+                .create_key(key_id, config);
+            match ecc {
+                Ok(_) => {
+                    let ausgabe = "ECC384 Schlüssel erfolgreich generiert";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+                Err(_) => {
+                    let ausgabe = "Fehler beim generieren des ECC384 Schlüssels";
+                    create_new_window2(app, ausgabe.to_string());
+                }
+            
+            }
+        }
+        _ => {}
+    }
 }
