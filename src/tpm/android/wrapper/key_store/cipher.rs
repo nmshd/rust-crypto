7f13bc9b7ee7f4c1535853dc5e3bef7020ca0647
mark-beck <67227500+mark-beck@users.noreply.github.com>
2024-06-10 16:38:09 +0200

    Team B Android: Change ConfigProvider -> Any in Linux, Windows and FFI and add symetric encryption for Android (#11)
    * feat: Add IvParameterSpec class and init2 method
    
    * feat: iv is saved in encryped data
    
    * fix: ProviderConf to Any and wrong args for initialize_module
    
    ---------
    
    Co-authored-by: markoisus <marko.nikolic34567@gmail.com>

--- src/tpm/android/wrapper/key_store/cipher.rs
+++ src/tpm/android/wrapper/key_store/cipher.rs
@@ -3,6 +3,7 @@ use robusta_jni::bridge;
 #[bridge]
 /// This module contains the JNI bindings for the Cipher class in the javax.crypto package.
 pub mod jni {
+    use crate::tpm::android::wrapper::key_generation::key::jni::Key;
     use robusta_jni::{
         convert::{IntoJavaValue, Signature, TryFromJavaValue, TryIntoJavaValue},
         jni::{
@@ -55,6 +56,35 @@ pub mod jni {
             opmode: i32,
             #[input_type("Ljava/security/Key;")] key: JObject,
         ) -> JniResult<()> {
+        }
+
+        pub fn init2(
+            &self,
+            env: &'borrow JNIEnv<'env>,
+            opmode: i32,
+            key: Key,
+            params: JObject,
+        ) -> JniResult<()> {
+            env.call_method(
+                self.raw.as_obj(),
+                "init",
+                "(ILjava/security/Key;Ljava/security/AlgorithmParameters;)V",
+                &[
+                    JValue::Int(opmode),
+                    JValue::Object(key.raw.as_obj()),
+                    JValue::Object(params),
+                ],
+            )?;
+            Ok(())
+        }
+
+        pub fn getIV(&self, env: &JNIEnv) -> JniResult<Vec<u8>> {
+            let output = env.call_method(self.raw.as_obj(), "getIV", "()[B", &[])?;
+
+            let output_array = output.l()?.into_inner() as jbyteArray;
+            let output_vec = env.convert_byte_array(output_array).unwrap();
+
+            Ok(output_vec)
         }
 
         /// Performs the final operation of the Cipher, processing any remaining data.
