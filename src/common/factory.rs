a039aba5179a1894a48ebc3618c7d6ca801d9942
Niklas Gussek <niklas.gussek@js-soft.com>
2024-08-06 13:29:18 +0200

    fix: add missing unimplemented as match branch

--- src/common/factory.rs
+++ src/common/factory.rs
@@ -26,7 +26,6 @@ pub enum SecurityModule {
     Tpm(TpmType),
     #[cfg(feature = "nks")]
     Nks,
-
 }
 
 /// Provides conversion from a string slice to a `SecurityModule` variant.
@@ -135,9 +134,11 @@ impl SecModule {
             SecurityModule::Hsm(hsm_type) => Some(HsmInstance::create_instance(key_id, hsm_type)),
             #[cfg(feature = "tpm")]
             SecurityModule::Tpm(tpm_type) => Some(TpmInstance::create_instance(key_id, tpm_type)),
-            // _ => unimplemented!(),
             #[cfg(feature = "nks")]
-            SecurityModule::Nks => Some(Arc::new(Mutex::new(crate::nks::hcvault::NksProvider::new(key_id)))),
+            SecurityModule::Nks => Some(Arc::new(Mutex::new(
+                crate::nks::hcvault::NksProvider::new(key_id),
+            ))),
+            _ => unimplemented!(),
         }
     }
 }
