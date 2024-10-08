cff3d72ef020499f2975fa87ab29a0cb2ef3a396
Mark Beck <67227500+mark-beck@users.noreply.github.com>
2024-10-08 13:21:30 +0200

    fix: changes for compatability with flutter-rust-bridge (#29)

--- src/common/traits/key_handle.rs
+++ src/common/traits/key_handle.rs
@@ -1,33 +1,40 @@
-use crate::common::{error::SecurityModuleError, DHExchange};
 use async_trait::async_trait;
-use std::fmt::Debug;
-#[cfg(feature = "linux")]
-use tss_esapi::handles::KeyHandle as TssKeyHandle;
-#[cfg(feature = "win")]
-use windows::Win32::Security::Cryptography::NCRYPT_KEY_HANDLE;
 
-/// An enum representing a generic key handle that can be used on different platforms.
+use crate::common::{error::SecurityModuleError, DHExchange};
+
+/// Defines a common interface for cryptographic key operations.
 ///
-/// This enum provides a platform-agnostic way to handle cryptographic keys. It has two variants:
-///
-/// - `Linux`: Used on Linux platforms, wrapping the `TssKeyHandle` type from the `tss_esapi` crate.
-/// - `Windows`: Used on Windows platforms, wrapping the `NCRYPT_KEY_HANDLE` type from the `windows` crate.
-///
-/// By using this enum, you can write code that works with cryptographic keys on both Linux and Windows platforms
-/// without having to worry about the underlying platform-specific types.
-#[repr(C)]
-#[cfg(any(feature = "linux", feature = "win"))]
-pub enum GenericKeyHandle {
-    #[cfg(feature = "linux")]
-    Linux(TssKeyHandle),
-    #[cfg(feature = "win")]
-    Windows(NCRYPT_KEY_HANDLE),
-    #[cfg(feature = "yubi")]
-    YubiKey(Box<dyn KeyHandleImpl>),
+/// This trait specifies methods for key operations such as signing data, encrypting,
+/// decrypting, and verifying signatures. It's designed to be implemented by security
+/// modules that manage cryptographic keys, ensuring a consistent interface for key
+/// operations across different types of security modules. Implementors of this trait
+/// must ensure thread safety.
+
+#[async_trait]
+pub trait KeyHandleImpl: Send + Sync {
+    /// Encrypts the given data using the cryptographic key.
+    ///
+    /// # Arguments
+    /// * `data` - A byte slice representing the data to be encrypted.
+    ///
+    /// # Returns
+    /// A `Result` containing the encrypted data as a `Vec<u8>` on success, or a `SecurityModuleError` on failure.
+    async fn encrypt_data(&self, data: &[u8]) -> Result<Vec<u8>, SecurityModuleError>;
+
+    /// Decrypts the given encrypted data using the cryptographic key.
+    ///
+    /// # Arguments
+    /// * `encrypted_data` - A byte slice representing the data to be decrypted.
+    ///
+    /// # Returns
+    /// A `Result` containing the decrypted data as a `Vec<u8>` on success, or a `SecurityModuleError` on failure.
+    async fn decrypt_data(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, SecurityModuleError>;
+
+    async fn extract_key(&self) -> Result<Vec<u8>, SecurityModuleError>;
 }
 
 #[async_trait]
-pub trait ISignVerify: Debug {
+pub trait KeyPairHandleImpl: Send + Sync {
     /// Signs the given data using the cryptographic key.
     ///
     /// # Arguments
@@ -51,10 +58,7 @@ pub trait ISignVerify: Debug {
         data: &[u8],
         signature: &[u8],
     ) -> Result<bool, SecurityModuleError>;
-}
 
-#[async_trait]
-pub trait IEncryptDecrypt: Debug {
     /// Encrypts the given data using the cryptographic key.
     ///
     /// # Arguments
@@ -72,38 +76,21 @@ pub trait IEncryptDecrypt: Debug {
     /// # Returns
     /// A `Result` containing the decrypted data as a `Vec<u8>` on success, or a `SecurityModuleError` on failure.
     async fn decrypt_data(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, SecurityModuleError>;
-}
-
-/// Defines a common interface for cryptographic key operations.
-///
-/// This trait specifies methods for key operations such as signing data, encrypting,
-/// decrypting, and verifying signatures. It's designed to be implemented by security
-/// modules that manage cryptographic keys, ensuring a consistent interface for key
-/// operations across different types of security modules. Implementors of this trait
-/// must ensure thread safety.
-
-#[async_trait]
-pub trait KeyHandleImpl: IEncryptDecrypt {
-    async fn extract_key(&self) -> Result<Vec<u8>, SecurityModuleError>;
-}
-
-#[async_trait]
-pub trait KeyPairHandleImpl: IEncryptDecrypt + ISignVerify {
     async fn get_public_key(&self) -> Result<Vec<u8>, SecurityModuleError>;
     async fn extract_key(&self) -> Result<Vec<u8>, SecurityModuleError>;
-    async fn start_dh_exchange(&self) -> Result<DHExchange, SecurityModuleError>;
+    fn start_dh_exchange(&self) -> Result<DHExchange, SecurityModuleError>;
 }
 
 #[async_trait]
-pub trait DHKeyExchangeImpl {
+pub trait DHKeyExchangeImpl: Send + Sync {
     /// Get the public key of the internal key pair to use for the other party
     async fn get_public_key(&self) -> Result<Vec<u8>, SecurityModuleError>;
 
     /// add an external public point and compute the shared secret. The raw secret is returned to use in another round of the key exchange
-    fn add_external(&mut self, external_key: &[u8]) -> Result<Vec<u8>, SecurityModuleError>;
+    async fn add_external(&mut self, external_key: &[u8]) -> Result<Vec<u8>, SecurityModuleError>;
 
     /// add the final external Keypair, derive a symmetric key from the shared secret and store the key
-    fn add_external_final(
+    async fn add_external_final(
         self,
         external_key: &[u8],
     ) -> Result<Box<dyn KeyHandleImpl>, SecurityModuleError>;
--- src/common/traits/module_provider.rs
+++ src/common/traits/module_provider.rs
@@ -1,9 +1,10 @@
+use async_trait::async_trait;
+
 use crate::common::{
     config::{KeyPairSpec, KeySpec, ProviderConfig, ProviderImplConfig},
     error::SecurityModuleError,
     DHExchange, KeyHandle, KeyPairHandle,
 };
-use async_trait::async_trait;
 
 #[async_trait]
 pub trait ProviderFactory {
@@ -21,7 +22,7 @@ pub trait ProviderFactory {
 /// This trait encapsulates operations related to cryptographic key creation and storage. It ensures a unified approach to interacting with different types
 /// of security modules.
 #[async_trait]
-pub trait ProviderImpl {
+pub trait ProviderImpl: Send + Sync {
     /// Creates a new symmetric key identified by `key_id`.
     ///
     /// # Arguments
