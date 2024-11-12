987e1631838359beff3dd1a09c1a16c37921f2d4
Adam McKellar <dev@mckellar.eu>
2024-11-12 17:45:19 +0100

    test(apple secure enclave): removed some unneeded entitlements, added print to shell script

--- src/tests/tpm/apple_secure_enclave/execute_test.ps1
+++ src/tests/tpm/apple_secure_enclave/execute_test.ps1
@@ -3,12 +3,18 @@ param(
     [String]$SigningKeyHash
 )
 
+Write-Host "Compiling Debug Test Build"
 [string]$cargo_test_output = cargo test -F apple-secure-enclave --no-default-features --no-run 2>&1
 $path_regex = [regex]"target\/debug\/deps\/crypto_layer-[a-z0-9]*"
 
 if ($cargo_test_output -match $path_regex) {
     $debug_binary_path = $Matches[0]
+    Write-Host $debug_binary_path
+    
+    Write-Host "Signing"
     & codesign -f -s $SigningKeyHash --entitlements binary-knights.entitlement -o runtime -i "de.jssoft.BinaryKnights" $debug_binary_path
+
+    Write-Host "Executing Tests"
     & $debug_binary_path
 }
 else {
