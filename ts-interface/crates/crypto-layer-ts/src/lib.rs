3a7c6b9417786c58625d06cbf22c6f9176dbf6f0
Adam McKellar <dev@mckellar.eu>
2024-11-22 16:59:28 +0100

    refactor(ts interface): switched to ts types package for types

--- ts-interface/crates/crypto-layer-ts/src/lib.rs
+++ ts-interface/crates/crypto-layer-ts/src/lib.rs
@@ -2,14 +2,10 @@ use neon::prelude::*;
 
 use crypto_layer::prelude::*;
 
-fn wrap_string<'a>(cx: &mut impl Context<'a>, s: String) -> Handle<'a, JsString> {
-    JsString::new(cx, s)
-}
-
 fn wrap_string_array<'a>(cx: &mut impl Context<'a>, arr: Vec<String>) -> JsResult<'a, JsArray> {
     let result = JsArray::new(cx, arr.len());
     for ( i, s) in arr.into_iter().enumerate() {
-        let js_s = wrap_string(cx,s);
+        let js_s = JsString::new(cx, s);
         result.set(cx, i as u32, js_s)?;
     }
 
@@ -20,13 +16,9 @@ fn export_get_all_providers(mut cx: FunctionContext) -> JsResult<JsArray> {
     wrap_string_array(&mut cx, get_all_providers())
 }
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
-}
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
     cx.export_function("getAllProviders", export_get_all_providers)?;
     Ok(())
 }
--- ts-interface/src/index.cts
+++ ts-interface/src/index.cts
@@ -3,12 +3,14 @@
 // The Rust addon.
 export * from './load.cjs';
 
-export * from "./types/index";
+export * from "crypto-layer-ts-types";
+
+import { type ProviderConfig, type ProviderImplConfig, type Provider } from "crypto-layer-ts-types";
 
 // Use this declaration to assign types to the addon's exports,
 // which otherwise by default are `any`.
 declare module "./load.cjs" {
-  export function hello(): string;
   export function getAllProviders(): string[];
+  export function createProvider(conf: ProviderConfig, impl_conf: ProviderImplConfig): Provider | null;
 }
 
