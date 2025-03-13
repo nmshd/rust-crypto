d683f38c3b00d83bb710c5091ab0c143ba6dfd5b
Adam McKellar <dev@mckellar.eu>
2025-03-13 13:59:39 +0100

    chore(ts types)!: updated types to changes
    Added biome liniting and fmt.

--- ts-types/manual/DHExchange.ts
+++ ts-types/manual/DHExchange.ts
@@ -1,7 +1,20 @@
-import { KeyHandle } from "./";
+import type { KeyHandle } from "./index.ts";
 
+// biome-ignore lint/style/useNamingConvention: <explanation>
 export type DHExchange = {
-    getPublicKey: () => Promise<Uint8Array>;
-    addExternal: (externalKey: Uint8Array) => Promise<Uint8Array>;
-    addExternalFinal: (externalKey: Uint8Array) => Promise<KeyHandle>;
+	getPublicKey: () => Promise<Uint8Array>;
+	// addExternal: (externalKey: Uint8Array) => Promise<Uint8Array>;
+	// addExternalFinal: (externalKey: Uint8Array) => Promise<KeyHandle>;
+	deriveClientSessionKeys: (
+		serverPk: Uint8Array,
+	) => Promise<[Uint8Array, Uint8Array]>;
+	deriveServerSessionKeys: (
+		clientPk: Uint8Array,
+	) => Promise<[Uint8Array, Uint8Array]>;
+	deriveClientKeyHandles: (
+		serverPk: Uint8Array,
+	) => Promise<[KeyHandle, KeyHandle]>;
+	deriveServerKeyHandles: (
+		clientPk: Uint8Array,
+	) => Promise<[KeyHandle, KeyHandle]>;
 };
