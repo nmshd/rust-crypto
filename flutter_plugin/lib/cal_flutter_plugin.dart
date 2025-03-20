0a11c3738a705957ee6d2ad4319c5cec41a8db2d
Mark Beck <67227500+mark-beck@users.noreply.github.com>
2025-03-20 17:50:03 +0100

    fix: export kdf in generated dart code (#115)

--- flutter_plugin/lib/cal_flutter_plugin.dart
+++ flutter_plugin/lib/cal_flutter_plugin.dart
@@ -5,6 +5,7 @@ export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/conf
 export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms.dart';
 export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms/encryption.dart';
 export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms/hashes.dart';
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms/key_derivation.dart';
 export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/factory.dart';
 
 export 'src/rust/api/crypto.dart';
