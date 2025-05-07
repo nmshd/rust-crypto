ebdfbaf2759827012051c4f5b13fbce44b7a9b75
Adam McKellar <dev@mckellar.eu>
2025-05-07 17:47:37 +0200

    chore: generated dart bindings

--- flutter_plugin/lib/src/rust/third_party/crypto_layer/common/crypto/algorithms/hashes.dart
+++ flutter_plugin/lib/src/rust/third_party/crypto_layer/common/crypto/algorithms/hashes.dart
@@ -6,7 +6,7 @@
 import '../../../../../frb_generated.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
-// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `assert_receiver_is_total_eq`, `clone`, `eq`, `fmt`, `from_str`, `from`, `from`, `hash`, `try_from`
+// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `assert_receiver_is_total_eq`, `clone`, `eq`, `fmt`, `from_str`, `from`, `from`, `hash`, `try_from`, `zeroize`
 
 /// Represents the available hashing algorithms.
 ///
