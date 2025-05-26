cbd63c8760551e1334f57c588237712b613dfe11
Mark Beck <67227500+mark-beck@users.noreply.github.com>
2025-05-26 19:34:57 +0200

    refactor(flutter_plugin):  rebuild flutter plugin in new folder, fix bug in cargokit (#144)

---
+++ cal_flutter_plugin/lib/cal_flutter_plugin.dart
@@ -0,0 +1,11 @@
+library;
+
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common.dart';
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/config.dart';
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms/encryption.dart';
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms/hashes.dart';
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/crypto/algorithms/key_derivation.dart';
+export 'package:cal_flutter_plugin/src/rust/third_party/crypto_layer/common/factory.dart';
+
+export 'src/rust/api/crypto.dart';
+export 'src/rust/frb_generated.dart' show RustLib;
