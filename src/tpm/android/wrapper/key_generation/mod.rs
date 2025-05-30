48c674d838b15df856456a593a0db84a86452b89
Mark Beck <mark-beck@web.de>
2025-05-31 00:34:21 +0200

    feat(android): add key derivation for Android

--- src/tpm/android/wrapper/key_generation/mod.rs
+++ src/tpm/android/wrapper/key_generation/mod.rs
@@ -7,5 +7,7 @@ pub(crate) mod key_gen_parameter_spec;
 pub(crate) mod key_generator;
 pub(crate) mod key_pair;
 pub(crate) mod key_pair_generator;
+pub(crate) mod key_protection;
+pub(crate) mod protections_builder;
 pub(crate) mod secret_key_spec;
 pub(crate) mod secure_random;
