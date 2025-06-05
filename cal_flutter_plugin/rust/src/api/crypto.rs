f1c8ff59b8a92a82c2b2be2546d9d811e3769469
Mark Beck <67227500+mark-beck@users.noreply.github.com>
2025-06-05 02:38:53 +0200

    feat(cal_flutter_plugin): add createWithKvConfig function (#147)

--- cal_flutter_plugin/rust/src/api/crypto.rs
+++ cal_flutter_plugin/rust/src/api/crypto.rs
@@ -1,7 +1,7 @@
 use crypto_layer::common::config::ProviderImplConfig;
 
-use std::sync::Arc;
 use flutter_rust_bridge::DartFnFuture;
+use std::sync::Arc;
 
 pub async fn get_default_config(
     get_fn: impl Fn(String) -> DartFnFuture<Option<Vec<u8>>> + 'static + Send + Sync,
@@ -9,6 +9,27 @@ pub async fn get_default_config(
     delete_fn: impl Fn(String) -> DartFnFuture<()> + 'static + Send + Sync,
     all_keys_fn: impl Fn() -> DartFnFuture<Vec<String>> + 'static + Send + Sync,
 ) -> ProviderImplConfig {
+    ProviderImplConfig::new(
+        Arc::new(get_fn),
+        Arc::new(store_fn),
+        Arc::new(delete_fn),
+        Arc::new(all_keys_fn),
+        vec![],
+    )
+}
 
-    ProviderImplConfig::new(Arc::new(get_fn), Arc::new(store_fn), Arc::new(delete_fn), Arc::new(all_keys_fn), vec![])
+pub async fn createWithKVConfig(
+    get_fn: impl Fn(String) -> DartFnFuture<Option<Vec<u8>>> + 'static + Send + Sync,
+    store_fn: impl Fn(String, Vec<u8>) -> DartFnFuture<bool> + 'static + Send + Sync,
+    delete_fn: impl Fn(String) -> DartFnFuture<()> + 'static + Send + Sync,
+    all_keys_fn: impl Fn() -> DartFnFuture<Vec<String>> + 'static + Send + Sync,
+    additional_config: Vec<AdditionalConfig>,
+) -> ProviderImplConfig {
+    ProviderImplConfig::new(
+        Arc::new(get_fn),
+        Arc::new(store_fn),
+        Arc::new(delete_fn),
+        Arc::new(all_keys_fn),
+        additional_config,
+    )
 }
