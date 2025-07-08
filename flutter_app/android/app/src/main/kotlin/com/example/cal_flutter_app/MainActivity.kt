bef012f3a1dd34cbb1da789ddecb75d971b90ef0
Mark Beck <mark-beck@web.de>
2025-07-08 12:57:31 +0200

    fix(android): fix android JNI_onLoad() (#152)

--- flutter_app/android/app/src/main/kotlin/com/example/cal_flutter_app/MainActivity.kt
+++ flutter_app/android/app/src/main/kotlin/com/example/cal_flutter_app/MainActivity.kt
@@ -1,46 +1,39 @@
 package com.example.cal_flutter_app
 
-import android.content.Context
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(
-            @NonNull flutterEngine: FlutterEngine,
-    ) {
-        super.configureFlutterEngine(flutterEngine)
-        flutterEngine.plugins.add(MyPlugin())
-    }
+    // override fun configureFlutterEngine(
+    //         @NonNull flutterEngine: FlutterEngine,
+    // ) {
+    //     super.configureFlutterEngine(flutterEngine)
+    //     flutterEngine.plugins.add(MyPlugin())
+    // }
 }
 
-class MyPlugin : FlutterPlugin, MethodCallHandler {
-    companion object {
-        init {
-            System.loadLibrary("cal_flutter_plugin")
-        }
-    }
+// class MyPlugin : FlutterPlugin, MethodCallHandler {
+//     companion object {
+//         init {
+//             System.loadLibrary("cal_flutter_plugin")
+//         }
+//     }
 
-    external fun init_android(ctx: Context)
+//     external fun init_android(ctx: Context)
 
-    override fun onAttachedToEngine(
-            @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding,
-    ) {
-        init_android(flutterPluginBinding.applicationContext)
-    }
+//     override fun onAttachedToEngine(
+//             @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding,
+//     ) {
+//         init_android(flutterPluginBinding.applicationContext)
+//     }
 
-    override fun onMethodCall(
-            @NonNull call: MethodCall,
-            @NonNull result: Result,
-    ) {
-        result.notImplemented()
-    }
+//     override fun onMethodCall(
+//             @NonNull call: MethodCall,
+//             @NonNull result: Result,
+//     ) {
+//         result.notImplemented()
+//     }
 
-    override fun onDetachedFromEngine(
-            @NonNull binding: FlutterPlugin.FlutterPluginBinding,
-    ) {}
-}
+//     override fun onDetachedFromEngine(
+//             @NonNull binding: FlutterPlugin.FlutterPluginBinding,
+//     ) {}
+// }
