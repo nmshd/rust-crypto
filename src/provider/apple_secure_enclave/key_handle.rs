9a36760e3fa4913103fec7b0ef0e9753be06d54f
Adam McKellar <dev@mckellar.eu>
2025-07-16 13:44:05 +0200

    feat(apple): public keys are loaded with private keys and not on demand, check if baseline algorithms work, switched to ecies cofactor

--- src/provider/apple_secure_enclave/key_handle.rs
+++ src/provider/apple_secure_enclave/key_handle.rs
@@ -7,6 +7,8 @@ use security_framework::key::Algorithm;
 use security_framework::key::SecKey;
 use security_framework_sys::key::kSecKeyOperationTypeDecrypt;
 use security_framework_sys::key::kSecKeyOperationTypeEncrypt;
+use security_framework_sys::key::kSecKeyOperationTypeSign;
+use security_framework_sys::key::kSecKeyOperationTypeVerify;
 use security_framework_sys::key::SecKeyAlgorithm;
 use security_framework_sys::key::SecKeyIsAlgorithmSupported;
 use security_framework_sys::key::SecKeyOperationType;
@@ -56,16 +58,16 @@ fn encryption_algorithm_from_spec(spec: &KeyPairSpec) -> Result<Algorithm, CalEr
 
         match spec.signing_hash {
             CryptoHash::Sha2_224 => {
-                Ok(Algorithm::ECIESEncryptionStandardVariableIVX963SHA224AESGCM)
+                Ok(Algorithm::ECIESEncryptionCofactorVariableIVX963SHA224AESGCM)
             }
             CryptoHash::Sha2_256 => {
-                Ok(Algorithm::ECIESEncryptionStandardVariableIVX963SHA256AESGCM)
+                Ok(Algorithm::ECIESEncryptionCofactorVariableIVX963SHA256AESGCM)
             }
             CryptoHash::Sha2_384 => {
-                Ok(Algorithm::ECIESEncryptionStandardVariableIVX963SHA384AESGCM)
+                Ok(Algorithm::ECIESEncryptionCofactorVariableIVX963SHA384AESGCM)
             }
             CryptoHash::Sha2_512 => {
-                Ok(Algorithm::ECIESEncryptionStandardVariableIVX963SHA512AESGCM)
+                Ok(Algorithm::ECIESEncryptionCofactorVariableIVX963SHA512AESGCM)
             }
             _ => Err(CalError::bad_parameter(
                 format!(
@@ -87,36 +89,16 @@ fn encryption_algorithm_from_spec(spec: &KeyPairSpec) -> Result<Algorithm, CalEr
 
 #[derive(Clone)]
 pub(crate) struct AppleSecureEnclaveKeyPair {
-    pub(super) key_handle: SecKey,
+    pub(super) private_key: SecKey,
+    pub(super) public_key: SecKey,
     pub(super) spec: KeyPairSpec,
     pub(super) storage_manager: Option<StorageManager>,
-}
-
-impl AppleSecureEnclaveKeyPair {
-    fn algorithm_supported(
-        &self,
-        operation: SecKeyOperationType,
-        algorithm: SecKeyAlgorithm,
-    ) -> bool {
-        let supported = unsafe {
-            SecKeyIsAlgorithmSupported(self.key_handle.as_concrete_TypeRef(), operation, algorithm)
-        };
-        supported != 0
-    }
-
-    fn decryption_algorithm_supported(&self, algorithm: Algorithm) -> bool {
-        self.algorithm_supported(kSecKeyOperationTypeDecrypt, algorithm.into())
-    }
-
-    /* fn ecryption_algorithm_supported(&self, algorithm: Algorithm) -> bool {
-        self.algorithm_supported(kSecKeyOperationTypeEncrypt, algorithm.into())
-    } */
 }
 
 impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
     #[instrument(level = "trace", skip(data))]
     fn sign_data(&self, data: &[u8]) -> Result<Vec<u8>, CalError> {
-        self.key_handle
+        self.private_key
             .create_signature(
                 signature_algorithm_from_crypto_hash(self.spec.signing_hash)?,
                 data,
@@ -126,11 +108,7 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
 
     #[instrument(level = "trace", skip(data, signature))]
     fn verify_signature(&self, data: &[u8], signature: &[u8]) -> Result<bool, CalError> {
-        let public_key: SecKey = self.key_handle.public_key().ok_or(CalError::missing_key(
-            "SecKeyCopyPublicKey returned NULL".to_owned(),
-            KeyType::Public,
-        ))?;
-        public_key
+        self.public_key
             .verify_signature(
                 signature_algorithm_from_crypto_hash(self.spec.signing_hash)?,
                 data,
@@ -143,7 +121,7 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
     fn encrypt_data(&self, data: &[u8]) -> Result<Vec<u8>, CalError> {
         let algorithm = encryption_algorithm_from_spec(&self.spec)?;
 
-        let public_key: SecKey = self.key_handle.public_key().ok_or(CalError::missing_key(
+        let public_key: SecKey = self.private_key.public_key().ok_or(CalError::missing_key(
             "SecKeyCopyPublicKey returned NULL".to_owned(),
             KeyType::Public,
         ))?;
@@ -160,7 +138,7 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
     #[instrument(level = "trace", skip(encrypted_data))]
     fn decrypt_data(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, CalError> {
         let algorithm = encryption_algorithm_from_spec(&self.spec)?;
-        self.key_handle
+        self.private_key
             .decrypt_data(algorithm, encrypted_data)
             .map_err(|err| {
                 CalError::failed_operation(
@@ -173,7 +151,7 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
 
     #[instrument(level = "trace", skip_all)]
     fn get_public_key(&self) -> Result<Vec<u8>, CalError> {
-        let public_key: SecKey = self.key_handle.public_key().ok_or(CalError::missing_key(
+        let public_key: SecKey = self.private_key.public_key().ok_or(CalError::missing_key(
             "SecKeyCopyPublicKey returned NULL".to_owned(),
             KeyType::Public,
         ))?;
@@ -198,7 +176,7 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
 
     #[instrument(level = "trace", skip_all)]
     fn id(&self) -> Result<String, CalError> {
-        match self.key_handle.application_label() {
+        match self.private_key.application_label() {
             None => Err(CalError::missing_value(
                 "kSecAttrApplicationLabel missing for this key".to_owned(),
                 false,
@@ -213,7 +191,7 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
         if let Some(storage_manager) = &self.storage_manager {
             storage_manager.delete(self.id()?)?;
         }
-        self.key_handle.delete().err_internal()
+        self.private_key.delete().err_internal()
     }
 
     fn spec(&self) -> KeyPairSpec {
@@ -221,10 +199,64 @@ impl KeyPairHandleImpl for AppleSecureEnclaveKeyPair {
     }
 }
 
+impl AppleSecureEnclaveKeyPair {
+    fn algorithm_supported(
+        key: &SecKey,
+        operation: SecKeyOperationType,
+        algorithm: SecKeyAlgorithm,
+    ) -> bool {
+        let supported =
+            unsafe { SecKeyIsAlgorithmSupported(key.as_concrete_TypeRef(), operation, algorithm) };
+        supported != 0
+    }
+
+    fn ecryption_algorithm_supported(&self, algorithm: Algorithm) -> bool {
+        Self::algorithm_supported(
+            &self.public_key,
+            kSecKeyOperationTypeEncrypt,
+            algorithm.into(),
+        ) && Self::algorithm_supported(
+            &self.private_key,
+            kSecKeyOperationTypeDecrypt,
+            algorithm.into(),
+        )
+    }
+
+    fn signature_algorithm_supported(&self, algorithm: Algorithm) -> bool {
+        Self::algorithm_supported(
+            &self.public_key,
+            kSecKeyOperationTypeVerify,
+            algorithm.into(),
+        ) && Self::algorithm_supported(
+            &self.private_key,
+            kSecKeyOperationTypeSign,
+            algorithm.into(),
+        )
+    }
+
+    pub(super) fn baseline_supported(&self) -> Result<(), CalError> {
+        if !self.ecryption_algorithm_supported(
+            Algorithm::ECIESEncryptionCofactorVariableIVX963SHA256AESGCM,
+        ) {
+            return Err(CalError::unsupported_algorithm(stringify!(
+                Algorithm::ECIESEncryptionCofactorVariableIVX963SHA256AESGCM
+            )));
+        }
+
+        if !self.signature_algorithm_supported(Algorithm::ECDSASignatureMessageX962SHA256) {
+            return Err(CalError::unsupported_algorithm(stringify!(
+                Algorithm::ECDSASignatureMessageX962SHA256
+            )));
+        }
+
+        Ok(())
+    }
+}
+
 impl fmt::Debug for AppleSecureEnclaveKeyPair {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("AppleSecureEnclaveKeyPair")
-            .field("key_handle", &self.key_handle)
+            .field("key_handle", &self.private_key)
             .field("metadata", &self.spec)
             .finish()
     }
@@ -233,7 +265,7 @@ impl fmt::Debug for AppleSecureEnclaveKeyPair {
 impl Drop for AppleSecureEnclaveKeyPair {
     fn drop(&mut self) {
         if self.storage_manager.is_none() {
-            if let Err(e) = self.key_handle.delete() {
+            if let Err(e) = self.private_key.delete() {
                 tracing::warn!("Failed to delete ephemeral key on device: {:?}", e);
             }
         }
