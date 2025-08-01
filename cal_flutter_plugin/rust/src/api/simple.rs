99b6c9b00f3bc0c0f4e1591173c697f41e8d56c7
Adam McKellar <dev@mckellar.eu>
2025-08-01 18:08:50 +0200

    Fix: Flutter Plugin Compilation Failure on Apple Platforms (#166)
    * fix: copmile failing on apple platforms
    
    The compilation error stemmed from multiple loggers being used while one was expected.
    The flutter rust bridge includes a logger by default.
    
    * chore: flutter generated artifacts
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- cal_flutter_plugin/rust/src/api/simple.rs
+++ cal_flutter_plugin/rust/src/api/simple.rs
@@ -1,9 +1,15 @@
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
     // Default utilities - feel free to customize
-    flutter_rust_bridge::setup_default_user_utils();
+    // flutter_rust_bridge::setup_default_user_utils();
     set_up_logging();
 }
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+use tracing::level_filters::LevelFilter;
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+use tracing_subscriber::Layer;
 
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::Registry;
@@ -14,7 +20,10 @@ pub(super) fn set_up_logging() {
         .with(tracing_android::layer("RUST").expect("could not create android logger"));
 
     #[cfg(any(target_os = "macos", target_os = "ios"))]
-    let subscriber = Registry::default().with(tracing_oslog::OsLogger::default());
+    let subscriber = Registry::default().with(
+        tracing_oslog::OsLogger::new("eu.nmshd.crypto-layer", "default")
+            .with_filter(LevelFilter::TRACE),
+    );
 
     #[cfg(not(any(target_os = "android", target_os = "macos", target_os = "ios")))]
     let subscriber = Registry::default();
